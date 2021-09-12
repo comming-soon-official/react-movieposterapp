c68256b27e6a8940beff7678b3d6053bb933ead6
comming-soon <himal9626@gmail.com>
2021-09-12 12:29:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./styles.css";
+import React from "react";
+import Home from "./component/Home"
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+
+function App(){
+  return(
+    <>
+    
+    
+    <Home />
+    
+    
+    
+    </>
+  )
+
+
 }
+export default App

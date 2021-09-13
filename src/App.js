af94588760c82139d9fb4334d3a6975073ab6302
comming-soon <himal9626@gmail.com>
2021-09-13 21:38:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Home from "./component/Home"
+import "./styles.css"
 
 
 function App(){
   return(
-    <>
+    <div style="background-color:black;">
     
     
     <Home />
